dd7789a9428ab74436112409c9ceb8c5740ffd24
ostr00000 <ostr00000@gmail.com>
2017-05-23 00:50:47 +0200

    add supervisor module

---
+++ sup.erl
@@ -0,0 +1,80 @@
+%%%-------------------------------------------------------------------
+%%% @author ostro
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 22. May 2017 23:23
+%%%-------------------------------------------------------------------
+-module(sup).
+-author("ostro").
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start_link_shell/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+start_link_shell() ->
+  {ok, Pid} = supervisor:start_link({global, ?MODULE}, ?MODULE, []),
+  unlink(Pid).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+  {ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
+    MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
+    [ChildSpec :: supervisor:child_spec()]
+  }} |
+  ignore |
+  {error, Reason :: term()}).
+init([]) ->
+  RestartStrategy = one_for_one,
+  MaxRestarts = 10,
+  MaxSecondsBetweenRestarts = 3600,
+
+  SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+  Restart = permanent,
+  Shutdown = 2000,
+  Type = worker,
+
+  AChild = {'rPol', {'rPollution', start_link, []},
+    Restart, Shutdown, Type, ['rPollution']},
+
+  {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
